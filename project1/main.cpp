c810ecbd3ee19b19de8bd7ca30abe2522743fbae
2018008395 <pch6828@naver.com>
2021-09-25 21:54:45 +0900

    project1 : make inter parallel join

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,25 +1,59 @@
 #include <iostream>
+#include <vector>
+#include <pthread.h>
 #include "Joiner.hpp"
 #include "Parser.hpp"
 
 using namespace std;
 //---------------------------------------------------------------------------
-int main(int argc, char* argv[]) {
-   Joiner joiner;
+Joiner joiner;
+//---------------------------------------------------------------------------
+
+void *thread_func(void *arg)
+{
+   QueryInfo &i = *((QueryInfo *)arg);
+   string *result = new string(joiner.join(i));
+   pthread_exit((void *)result);
+}
+//---------------------------------------------------------------------------
+int main(int argc, char *argv[])
+{
    // Read join relations
    string line;
-   while (getline(cin, line)) {
-      if (line == "Done") break;
+   while (getline(cin, line))
+   {
+      if (line == "Done")
+         break;
       joiner.addRelation(line.c_str());
    }
    // Preparation phase (not timed)
    // Build histograms, indexes,...
    //
-   QueryInfo i;
-   while (getline(cin, line)) {
-      if (line == "F") continue; // End of a batch
-      i.parseQuery(line);
-      cout << joiner.join(i);
+   vector<pthread_t *> threads;
+   while (getline(cin, line))
+   {
+      if (line == "F")
+      {
+         for (auto &thread : threads)
+         {
+            void *ret;
+            pthread_join(*thread, &ret);
+            cout << *((string *)ret);
+         }
+         threads.clear();
+         continue;
+      }
+
+      pthread_t *thread = new pthread_t();
+      threads.push_back(thread);
+
+      QueryInfo *i = new QueryInfo();
+      i->parseQuery(line);
+      if (pthread_create(thread, NULL, thread_func, i) < 0)
+      {
+         exit(-1);
+      }
    }
+
    return 0;
 }
