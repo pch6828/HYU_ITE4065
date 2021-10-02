0accd90aeea4634a7a17eca1946b1404c976f063
2018008395 <pch6828@naver.com>
2021-10-03 06:01:03 +0900

    project1 : rollback - undone parallel selfjoin

--- project1/Operators.cpp
+++ project1/Operators.cpp
@@ -371,48 +371,10 @@ void SelfJoin::run()
   auto leftCol = inputData[leftColId];
   auto rightCol = inputData[rightColId];
 
-  auto partitionSize = input->resultSize / NUM_PARTITION + 1;
-
-  auto selfjoin_on_partition = [&](uint64_t partitionId)
+  for (uint64_t i = 0; i < input->resultSize; ++i)
   {
-    vector<uint64_t> *subresult = new vector<uint64_t>();
-    uint64_t startIdx = partitionId * partitionSize;
-    uint64_t endIdx = min(startIdx + partitionSize, input->resultSize);
-    for (uint64_t i = startIdx; i < endIdx; ++i)
-    {
-      if (leftCol[i] == rightCol[i])
-      {
-        subresult->push_back(i);
-      }
-    }
-
-    return subresult;
-  };
-
-  vector<pthread_t *> threads;
-  for (uint64_t i = 0; i < NUM_PARTITION; i++)
-  {
-    pthread_t *thread = new pthread_t();
-    threads.push_back(thread);
-    join_thread_args *args = new join_thread_args();
-    args->func = &selfjoin_on_partition;
-    args->partitionId = i;
-    if (pthread_create(thread, NULL, join_thread_func<decltype(selfjoin_on_partition)>, (void *)args) < 0)
-    {
-      exit(-1);
-    }
-  }
-
-  for (auto &thread : threads)
-  {
-    vector<uint64_t> *ret;
-    pthread_join(*thread, (void **)&ret);
-
-    for (auto &id : *ret)
-    {
-      copy2Result(id);
-    }
-    delete ret;
+    if (leftCol[i] == rightCol[i])
+      copy2Result(i);
   }
 }
 //---------------------------------------------------------------------------
@@ -456,7 +418,7 @@ void Checksum::run()
 
   for (auto &thread : threads)
   {
-    void* ret;
+    void *ret;
     pthread_join(*thread, &ret);
 
     checkSums.push_back((uint64_t)ret);
