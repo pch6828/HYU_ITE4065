00e6e825c33aef19faee039090db87e5605c3ad3
2018008395 <pch6828@naver.com>
2021-10-05 15:21:12 +0900

    project 1 : construct basic thread pool

--- project1/main.cpp
+++ project1/main.cpp
@@ -6,14 +6,65 @@
 
 using namespace std;
 //---------------------------------------------------------------------------
+#define NUM_THREAD 23
+//---------------------------------------------------------------------------
 Joiner joiner;
+bool is_done = false;
+uint64_t num_active_thread = 0;
+QueryInfo *thread_arg[NUM_THREAD];
+string *volatile thread_ret[NUM_THREAD];
+pthread_t threads[NUM_THREAD];
+pthread_mutex_t mtx_for_workers[NUM_THREAD];
+pthread_cond_t cv_for_workers[NUM_THREAD];
 //---------------------------------------------------------------------------
 void *thread_func(void *arg)
 {
-   QueryInfo &i = *((QueryInfo *)arg);
-   string *result = new string(joiner.join(i));
-   pthread_exit((void *)result);
+   long tid = (long)arg;
+   pthread_mutex_lock(&mtx_for_workers[tid]);
+   thread_ret[tid] = (string *)-1;
+   pthread_cond_wait(&cv_for_workers[tid], &mtx_for_workers[tid]);
+   pthread_mutex_unlock(&mtx_for_workers[tid]);
+
+   while (!is_done)
+   {
+      QueryInfo &i = *(thread_arg[tid]);
+
+      string *result = new string(joiner.join(i));
+
+      pthread_mutex_lock(&mtx_for_workers[tid]);
+      thread_ret[tid] = result;
+      pthread_cond_wait(&cv_for_workers[tid], &mtx_for_workers[tid]);
+      pthread_mutex_unlock(&mtx_for_workers[tid]);
+   }
+
+   return nullptr;
 }
+
+void flush_all_thread()
+{
+   while (true)
+   {
+      bool all_threads_done = true;
+      for (long i = 0; i < num_active_thread; i++)
+      {
+         if (thread_ret[i] == (string *)-1)
+         {
+            all_threads_done = false;
+            break;
+         }
+      }
+      if (all_threads_done)
+      {
+         break;
+      }
+   }
+   for (long i = 0; i < num_active_thread; i++)
+   {
+      cout << *(thread_ret[i]);
+      thread_ret[i] = (string *)-1;
+   }
+}
+
 //---------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
@@ -28,30 +79,56 @@ int main(int argc, char *argv[])
    // Preparation phase (not timed)
    // Build histograms, indexes,...
    //
-   vector<pthread_t *> threads;
+
+   for (long i = 0; i < NUM_THREAD; i++)
+   {
+      mtx_for_workers[i] = PTHREAD_MUTEX_INITIALIZER;
+      cv_for_workers[i] = PTHREAD_COND_INITIALIZER;
+      if (pthread_create(&threads[i], 0, thread_func, (void *)i) < 0)
+      {
+         printf("pthread_create error!\n");
+         return 0;
+      }
+      while (thread_ret[i] != (string *)-1)
+      {
+         pthread_yield();
+      }
+   }
+
    while (getline(cin, line))
    {
       if (line == "F")
       {
-         for (auto &thread : threads)
-         {
-            void *ret;
-            pthread_join(*thread, &ret);
-            cout << *((string *)ret);
-         }
-         threads.clear();
+         flush_all_thread();
+         num_active_thread = 0;
          continue;
       }
 
-      pthread_t *thread = new pthread_t();
-      threads.push_back(thread);
-
       QueryInfo *i = new QueryInfo();
       i->parseQuery(line);
-      if (pthread_create(thread, NULL, thread_func, i) < 0)
+      if (num_active_thread == NUM_THREAD)
       {
-         exit(-1);
+         flush_all_thread();
+         num_active_thread = 0;
       }
+      thread_arg[num_active_thread] = i;
+      thread_ret[num_active_thread] = (string *)-1;
+      pthread_mutex_lock(&mtx_for_workers[num_active_thread]);
+      pthread_cond_broadcast(&cv_for_workers[num_active_thread]);
+      pthread_mutex_unlock(&mtx_for_workers[num_active_thread]);
+
+      num_active_thread++;
    }
+
+   // Wait threads end
+   is_done = true;
+   for (int i = 0; i < NUM_THREAD; i++)
+   {
+      pthread_mutex_lock(&mtx_for_workers[i]);
+      pthread_cond_broadcast(&cv_for_workers[i]);
+      pthread_mutex_unlock(&mtx_for_workers[i]);
+      pthread_join(threads[i], NULL);
+   }
+
    return 0;
 }
