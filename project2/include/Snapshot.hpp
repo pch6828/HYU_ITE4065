27afb8c043e33337cb59ec21b4047bc106d63464
2018008395 <pch6828@naver.com>
2021-10-15 23:20:20 +0900

    project2 : implemented Snapshot API

---
+++ project2/include/Snapshot.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <valarray>
+
+class SnapValue
+{
+private:
+    int label;
+    int value;
+    std::valarray<int> snap;
+
+public:
+    SnapValue() : label(0),
+                  value(0),
+                  snap(std::valarray<int>())
+    {
+        //Do Nothing
+    }
+    SnapValue(int label, int value, std::valarray<int> snap) : label(label),
+                                                               value(value),
+                                                               snap(snap)
+    {
+        //Do Nothing
+    }
+
+    int getLabel();
+    int getValue();
+    std::valarray<int> getSnap();
+};
+//---------------------------------------------------------------------------
+class Snapshot
+{
+private:
+    int numThread;
+    std::valarray<SnapValue *> regs;
+
+    std::valarray<SnapValue *> collect();
+
+public:
+    Snapshot(int numThread) : numThread(numThread),
+                              regs(std::valarray<SnapValue *>(numThread))
+    {
+        for (auto &reg : regs)
+        {
+            reg = new SnapValue();
+        }
+    }
+
+    std::valarray<int> scan();
+    void update(int tid, int value);
+};
---
+++ project2/src/Snapshot.cpp
@@ -0,0 +1,81 @@
+#include "Snapshot.hpp"
+#include <valarray>
+
+using namespace std;
+
+int SnapValue::getLabel()
+{
+    return this->label;
+}
+
+int SnapValue::getValue()
+{
+    return this->value;
+}
+
+valarray<int> SnapValue::getSnap()
+{
+    return this->snap;
+}
+//---------------------------------------------------------------------------
+static valarray<int> getValueFromRegisters(valarray<SnapValue *> &regs)
+{
+    valarray<int> result(regs.size());
+
+    for (int tid = 0; tid < regs.size(); tid++)
+    {
+        result[tid] = regs[tid]->getValue();
+    }
+
+    return result;
+}
+//---------------------------------------------------------------------------
+valarray<SnapValue *> Snapshot::collect()
+{
+    return this->regs;
+}
+
+valarray<int> Snapshot::scan()
+{
+    valarray<SnapValue *> oldCopy, newCopy;
+    valarray<bool> moved(numThread, false);
+
+    oldCopy = this->collect();
+
+    while (true)
+    {
+        bool flag = true;
+        newCopy = this->collect();
+        for (int tid = 0; tid < numThread; tid++)
+        {
+            if (oldCopy[tid]->getLabel() != newCopy[tid]->getLabel())
+            {
+                if (moved[tid])
+                {
+                    return newCopy[tid]->getSnap();
+                }
+                else
+                {
+                    moved[tid] = true;
+                    oldCopy = newCopy;
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if (flag)
+        {
+            break;
+        }
+    }
+
+    return getValueFromRegisters(newCopy);
+}
+
+void Snapshot::update(int tid, int value)
+{
+    valarray<int> snap = this->scan();
+    SnapValue *oldValue = regs[tid];
+    SnapValue *newValue = new SnapValue(oldValue->getLabel() + 1, value, snap);
+    regs[tid] = newValue;
+}
